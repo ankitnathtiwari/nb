d6d3e34dbc07a3124f21223d4dd6832528bc27e0
ankitnathtiwari <ankitfounder@gmail.com>
2021-07-17 16:13:02 +0530

    new build

--- src/app/base-url/index.js
+++ src/app/base-url/index.js
@@ -1,4 +1,5 @@
 export const prodUrl = "http://localhost:8000";
 export const testUrl = "http://localhost:8080";
 export const awsIpUrl = "http://3.17.55.187";
-export const baseUrl = awsIpUrl;
+export const awsProdUrl = "http://localhost";
+export const baseUrl = awsProdUrl;
