f6c4b27725487139c7b9a9cfb0ff5f6ac1053e08
ankitnathtiwari <ankitfounder@gmail.com>
2021-07-17 16:15:47 +0530

    url changed

--- src/app/base-url/index.js
+++ src/app/base-url/index.js
@@ -2,4 +2,4 @@ export const prodUrl = "http://localhost:8000";
 export const testUrl = "http://localhost:8080";
 export const awsIpUrl = "http://3.17.55.187";
 export const awsProdUrl = "http://localhost";
-export const baseUrl = awsProdUrl;
+export const baseUrl = awsIpUrl;
