e5aeff5d7efd3baa4791caa421048e8c04f84b2c
ankitnathtiwari <ankitfounder@gmail.com>
2021-07-17 16:07:19 +0530

    removed port

--- src/app/base-url/index.js
+++ src/app/base-url/index.js
@@ -1,4 +1,4 @@
 export const prodUrl = "http://localhost:8000";
 export const testUrl = "http://localhost:8080";
-export const awsIpUrl = "http://3.17.55.187:8000";
+export const awsIpUrl = "http://3.17.55.187";
 export const baseUrl = awsIpUrl;
