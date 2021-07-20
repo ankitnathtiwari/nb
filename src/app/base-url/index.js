44bf84ba75053c5c93f4c6ab866a854ee4305d59
ankitnathtiwari <ankitfounder@gmail.com>
2021-07-20 14:23:58 +0530

    see what hapens

--- src/app/base-url/index.js
+++ src/app/base-url/index.js
@@ -3,4 +3,4 @@ export const testUrl = "http://localhost:8080";
 export const awsIpUrl = "http://3.17.55.187";
 export const domain = "https://newsbird.live";
 export const awsProdUrl = "http://localhost";
-export const baseUrl = awsProdUrl;
+export const baseUrl = domain;
