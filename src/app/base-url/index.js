1e0cfcf68200dc6327267d0e57ea9c23b0c28554
ankitnathtiwari <ankitfounder@gmail.com>
2021-10-10 16:00:55 +0530

    url changed

--- src/app/base-url/index.js
+++ src/app/base-url/index.js
@@ -1,7 +1,8 @@
-export const prodUrl = "http://localhost:8000";
-export const testUrl = "http://localhost:8080";
+export const testUrl = "http://localhost:8000";
+export const mockUrl = "http://localhost:8080";
 export const awsIpUrl = "http://3.17.55.187";
-export const domain = "https://reporters.newsbird.live";
+export const nbdomain = "http://newsbird.live";
+export const reporterdomain = "https://reporters.newsbird.live";
 export const awsProdUrl = "http://localhost";
 
-export const baseUrl = prodUrl;
+export const baseUrl = nbdomain;
