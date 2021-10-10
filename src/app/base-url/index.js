53e07fc8ab3220bcd90d448a2e5e1f6c75b28e73
ankitnathtiwari <ankitfounder@gmail.com>
2021-10-10 16:34:31 +0530

    reporter build with https

--- src/app/base-url/index.js
+++ src/app/base-url/index.js
@@ -2,7 +2,8 @@ export const testUrl = "http://localhost:8000";
 export const mockUrl = "http://localhost:8080";
 export const awsIpUrl = "http://3.17.55.187";
 export const nbdomain = "http://newsbird.live";
-export const reporterdomain = "https://reporters.newsbird.live";
+export const snbdomain = "http://newsbird.live";
+export const sReporterDomain = "https://reporters.newsbird.live";
 export const awsProdUrl = "http://localhost";
 
-export const baseUrl = nbdomain;
+export const baseUrl = sReporterDomain;
