e99fe1979882416ff0be7430abfbc35163875858
ankitIndusOs <ankit.tiwari@indusos.com>
2021-12-16 23:12:21 +0530

    image url fixed

--- src/app/content/post-view/index.js
+++ src/app/content/post-view/index.js
@@ -4,8 +4,13 @@ import "./index.css";
 
 export const PostView = ({ post, style }) => {
   return (
-    <div className='post-view' style={style}>
-      <img src={`${imageUrl}/${post.image}`} alt='post image' />
+    <div className="post-view" style={style}>
+      <img
+        src={`${imageUrl}/${
+          post.image[0] === "/" ? post.image.substr(1) : post.image
+        }`}
+        alt="post image"
+      />
       <h2>{post.title}</h2>
       <p>{post.content}</p>
     </div>
