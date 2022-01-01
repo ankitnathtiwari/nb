458cb3e59f3eab5d776ac9fcc43404df79f7b8dd
ankitIndusOs <ankit.tiwari@indusos.com>
2022-01-01 14:02:37 +0530

    new url updated

--- src/app/base-url/index.js
+++ src/app/base-url/index.js
@@ -1,7 +1,7 @@
 export const localApiUrl = "http://localhost:8000";
 export const mockUrl = "http://localhost:8080";
 export const prodIp = "http://15.207.52.81";
-export const prodSecureUrl = "http://newsbird.live";
+export const prodSecureUrl = "https://newsbird.live";
 export const prodUrl = "http://newsbird.live";
 export const stagingUrl = "http://3.110.33.147";
 export const prodReporterUrl = "https://reporters.newsbird.live";
@@ -10,4 +10,4 @@ export const s3ImageUrl = "https://nb-post-image.s3.us-east-2.amazonaws.com"
 
 
 export const imageUrl = s3ImageUrl;
-export const baseUrl = prodSecureUrl;
+export const baseUrl = prodReporterUrl;
