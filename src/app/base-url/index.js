cc94675c978e8826ac989ab12eb4269a697cbbac
ankitIndusOs <ankit.tiwari@indusos.com>
2022-01-01 11:38:47 +0530

    new backend flow

--- src/app/base-url/index.js
+++ src/app/base-url/index.js
@@ -1,6 +1,6 @@
 export const localApiUrl = "http://localhost:8000";
 export const mockUrl = "http://localhost:8080";
-export const awsIpUrl = "http://3.17.55.187";
+export const prodIp = "http://15.207.52.81";
 export const prodSecureUrl = "http://newsbird.live";
 export const prodUrl = "http://newsbird.live";
 export const stagingUrl = "http://3.110.33.147";
@@ -10,4 +10,4 @@ export const s3ImageUrl = "https://nb-post-image.s3.us-east-2.amazonaws.com"
 
 
 export const imageUrl = s3ImageUrl;
-export const baseUrl = prodReporterUrl;
+export const baseUrl = prodIp;
