96b7387cec81ee589da59a1f0078ebceb6a874a9
ankitIndusOs <ankit.tiwari@indusos.com>
2022-01-18 23:55:52 +0530

    added one base url

--- src/app/base-url/index.js
+++ src/app/base-url/index.js
@@ -10,9 +10,15 @@ export const s3ImageUrl =
   "https://nb-post-image-mum.s3.ap-south-1.amazonaws.com";
 export const s3VideoUrl =
   "https://nb-test-video-mum.s3.ap-south-1.amazonaws.com";
-
 export const s3thumbnailUrl =
   "https://nb-test-video-mum.s3.ap-south-1.amazonaws.com";
+
+export const prodVideoUrl =
+  "https://nb-video-post-mum.s3.ap-south-1.amazonaws.com";
+export const prodImageUrl =
+  "https://nb-post-image-mum.s3.ap-south-1.amazonaws.com";
+export const prodthumbnailUrl =
+  "https://nb-video-thumbnail-mum.s3.ap-south-1.amazonaws.com";
 
 export const selectTopics = [
   { value: "politics", labelName: "Politics" },
@@ -26,7 +32,7 @@ export const selectTopics = [
   { value: "miscellaneous", labelName: "Miscellaneous" },
 ];
 
-export const imageUrl = s3ImageUrl;
-export const baseUrl = stagingUrl;
-export const videoUrl = s3VideoUrl;
-export const thumbnailUrl = s3thumbnailUrl;
+export const imageUrl = prodImageUrl;
+export const baseUrl = prodReporterUrl;
+export const videoUrl = prodVideoUrl;
+export const thumbnailUrl = prodthumbnailUrl;
